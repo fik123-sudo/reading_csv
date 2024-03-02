35a956f1190866925871e1e64b3fd8aaad4ded00
fik123-sudo <75900674+fik123-sudo@users.noreply.github.com>
2024-03-01 17:47:19 -0800

    Initial commit

---
+++ index.js
@@ -0,0 +1,18 @@
+const {parse} = require('csv-parse');
+const fs = require('fs');
+
+const result = [];
+
+fs.createReadStream('kepler_data.csv')
+.pipe(parse())
+.on('data',(data)=>{
+    result.push(data);
+
+})
+.on('error',(err)=>{
+    console.log(err);
+})
+.on('end',()=>{
+    console.log(result);
+    console.log('done');
+});
