29e0d95687fda761464f83d4e3a95e18f33c41be
fik123-sudo <75900674+fik123-sudo@users.noreply.github.com>
2024-03-02 16:32:11 -0800

    adding aprametrize URL

---
+++ httpservercration.js
@@ -0,0 +1,35 @@
+const http = require('http');
+const PORT = 3000;
+const server = http.createServer((req,res)=>{
+
+    const items = req.url.split('/');
+
+    if( items === '/freinds'){
+        res.writeHead(200,{
+            'content-type':'application/json',
+        });
+        res.end(JSON.stringify({
+            id:4574,
+            name: 'fikereyesus',
+        }));
+    }else if(req.url === '/messages'){
+        res.write('<html>');
+        res.write('<body>');
+        res.write('<ul>');
+        res.write('<li>this is messagnig server</li>');
+        res.write('</ul>');
+        res.write('</body >');
+        res.write('</html>');
+        res.statusCode(404);
+        res.end();
+
+    }
+    });
+
+    
+    
+    
+server.listen(PORT,()=>{
+    console.log(`server is listnning on port ${PORT}`)
+});
+
--- index.js
+++ index.js
@@ -2,10 +2,16 @@ const {parse} = require('csv-parse');
 const fs = require('fs');
 
 const result = [];
+function isHabitable(planet){
+    return planet[isHabitable] = 'CONFIRMED';
+}
 
 fs.createReadStream('kepler_data.csv')
 .pipe(parse())
 .on('data',(data)=>{
+    if(isHabitable(data)){
+
+    }
     result.push(data);
 
 })
